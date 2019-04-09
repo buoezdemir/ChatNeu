9d99a914d8ca584f6494fb7456ba896b1222f243
buoez <buoez@LAPTOP-0NB736IN>
2019-04-09 12:09:53 +0200

    Lukas Backup

---
+++ src/application/Client.java
@@ -0,0 +1,5 @@
+package application;
+
+public class Client {
+int neuner = 0;
+}
---
+++ src/application/Server.java
@@ -0,0 +1,44 @@
+package application;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+public class Server {
+
+	public static void main(String[] args) {
+		Server server = new Server(8000);
+	}
+
+	private int port;
+
+	public Server(int port) {
+		super();
+		this.port = port;
+	}
+public void startListening() {
+	new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			try {
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket remoteClientSocket = serverSocket.accept();
+			
+			Scanner scan = new Scanner(new Buffered Reader(new InputStreamReader(remoteClientSocket.getInputStream())))
+			if(s.hasNextLine()) {
+				System.out.println("Nachricht: "+ s.nextLine());
+			}
+			s.close();
+			remoteClientSocket.close();
+			ServerSocket.close();
+			} catch(Exeption e) {
+				e.printStackTrace();
+			}
+			}
+
+		}
+	})
+	
+}
+}
