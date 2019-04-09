2dd44241bfe53a34937d59047a85735d79a99e11
buoez <buoez@LAPTOP-0NB736IN>
2019-04-09 12:56:59 +0200

    Elias_Client

--- src/application/Client.java
+++ src/application/Client.java
@@ -1,5 +1,28 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
-int neuner = 0;
+	public static void main(String[] args) {
+		try {
+			Socket s = new Socket("127.0.0.1", 1201);
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String msgin="", msgout="";
+			while(!msgin.equals("ende")){
+				msgout = br.readLine();
+				dout.writeUTF(msgout);
+				msgin = din.readUTF();
+				System.out.println(msgin);
+			}
+		}catch(Exception e){
+			
+		}
+		}
 }
